549f99ae3e922313cbb8c16b18efd540dca21b13
Tommaso <tommiberto04@gmail.com>
2020-03-24 16:35:36 +0100

    main.py conta fino a 100

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("inmpariamo a contare analfabeta: ")
 for i in range(10):
 	print(a+1)
 	a+=1
+b=0
+print("ora che sei un pro gamer, coniamo fino a 100: ")
+for i in range(100):
+	print(b+1)
+	b+=1
+	
